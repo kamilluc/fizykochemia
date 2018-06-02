bae1fc050d2aa603fde8cf2912fa831c27ca9ef2
kamilluc <kamil.luc@outlook.com>
2018-06-02 15:44:02 +0200

    bb

--- static/pwabuilder-sw-register.js
+++ static/pwabuilder-sw-register.js
@@ -5,7 +5,7 @@ if (navigator.serviceWorker.controller) {
   console.log('[PWA Builder] active service worker found, no need to register')
 } else {
   //Register the ServiceWorker
-  navigator.serviceWorker.register('pwabuider-sw.js', {
+  navigator.serviceWorker.register('https://rawgit.com/kamilluc/fizykochemia/master/static/pwabuilder-sw.js', {
     scope: './'
   }).then(function(reg) {
     console.log('Service worker has been registered for scope:'+ reg.scope);
